544e65ae793538760a7f87095963c3b95cd560dd
Julia Sun <Julia Sun>
2015-08-16 21:34:06 -0700

    fixed page title update on reroute

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,32 @@ var app = angular.module('julia', [
         'ngTouch'
     ]);
 
+app.run(['$rootScope',
+    '$state',
+    '$location',
+    '$window',
+    function ($rootScope, $state, $location, $window) {
+        $rootScope.$state = $state;
+
+        // Get Page Title on successful page change
+        $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+            // Sets HTML/Page Title
+            $rootScope.currentPage = toState.name;
+
+            if (toState.data && toState.data.pageTitle) {
+                $rootScope.pageTitle = toState.data.pageTitle;
+            } else {
+                $rootScope.pageTitle = 'Julia Sun';
+            }
+        });
+
+        // Scroll to top of page on page change
+        $rootScope.$on('$viewContentLoaded', function() {
+            document.body.scrollTop = document.documentElement.scrollTop = 0;
+        });
+    }
+]);
+
 app.config(['$stateProvider',
     '$urlRouterProvider',
     '$locationProvider',
@@ -15,21 +41,21 @@ app.config(['$stateProvider',
                 url: '/',
                 templateUrl: './views/home.html',
                 data: {
-                    pageTitle: 'Title'
+                    pageTitle: 'Julia Sun'
                 }
             })
             .state('about', {
                 url: '/about',
                 templateUrl: './views/about.html',
                 data: {
-                    pageTitle: 'About'
+                    pageTitle: 'Julia | About'
                 }
             })
             .state('projects', {
                 url: '/projects',
                 templateUrl: './views/projects.html',
                 data: {
-                    pageTitle: 'Projects'
+                    pageTitle: 'Juila | Projects'
                 }
             });
     }
