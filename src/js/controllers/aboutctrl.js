aa750753bd8718e752686a3f05b6350b653c9df8
Julia Sun <Julia Sun>
2015-08-19 16:05:03 -0700

    rough outline of about page

--- src/js/controllers/aboutctrl.js
+++ src/js/controllers/aboutctrl.js
@@ -1,4 +1,35 @@
 app.controller('aboutctrl', ['$scope', function($scope) {
-    // Add functionality for home page here
-    $scope.greeting = 'Hello World';
+
+    $scope.descriptions = [
+        {
+            title: 'Julia Sun',
+            subtitle: 'filler',
+            photo: '../assets/img/about/prof.jpg',
+            blurb: 'Hi my name is Julia.'
+        },
+        {
+            title: 'Class of 2017',
+            subtitle: 'University of California, Berkeley',
+            photo: '../assets/img/about/campanile.jpg',
+            blurb: 'I am currently a third year computer science major.'
+        },
+        {
+            title: 'Engineering Practicum Intern',
+            subtitle: 'Google, Inc.',
+            photo: '../assets/img/about/google.jpg',
+            blurb: 'I was an EP intern at Google this past summer.'
+        },
+        {
+            title: 'Adoption Volunteer',
+            subtitle: 'Doggie Protective Services',
+            photo: '../assets/img/about/dps.jpg',
+            blurb: 'I love dogs!'
+        },
+        {
+            title: 'Poro Enthusiast',
+            subtitle: 'League of Legends',
+            photo: '../assets/img/about/poros.jpg',
+            blurb: 'I really like poros.'
+        }
+    ];
 }]);
