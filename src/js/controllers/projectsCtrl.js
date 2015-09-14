6596b9226250c55c0b3f4fcbb71c7da558b61db0
Julia Sun <Julia Sun>
2015-09-14 00:55:00 -0700

    making project page

--- src/js/controllers/projectsCtrl.js
+++ src/js/controllers/projectsCtrl.js
@@ -1,3 +1,13 @@
-app.controller('aboutCtrl', ['$scope', function($scope) {
+app.controller('projectsCtrl', ['$scope', function($scope) {
+
+    $scope.title = "Projects";
+
+    $scope.projects = [
+        {
+            link: "",
+            title: "Innovative Design Photo Portfolio",
+            preview: "../assets/img/projects/photo-portfolio-preview.png"
+        }
+    ];
 
 }]);
