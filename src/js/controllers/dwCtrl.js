e268cc10cbc6d32f51cbdd2d72f2d4a9ebb30792
Julia Sun <julia.sun@berkeley.edu>
2016-01-01 22:15:26 -0800

    device wall page done

---
+++ src/js/controllers/dwCtrl.js
@@ -0,0 +1,10 @@
+app.controller('dwCtrl', ['$scope', function($scope) {
+    $scope.title = "Device Wall";
+
+    $scope.images = [
+        '../assets/img/projects/device-wall/command-wall-1.png',
+        '../assets/img/projects/device-wall/command-wall-2.png',
+        '../assets/img/projects/device-wall/select-wall.png',
+        '../assets/img/projects/device-wall/select-wall-expanded.png'
+    ];
+}]);
